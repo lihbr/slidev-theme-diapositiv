2d62e74c46698c907865fa8f58531d2f9265432f
lihbr <lihbr@users.noreply.github.com>
2021-08-27 17:17:12 +0200

    feat: update footnote style

--- components/Footnote.vue
+++ components/Footnote.vue
@@ -1,7 +1,23 @@
 <template>
-	<footer class="footnote px-10 pb-10 absolute bottom-0 left-0 w-full text-xs font-300 text-cream-100 hover:text-slate dark:(text-slate-100 hover:text-cream) transition-colors">
+	<footer
+		class="
+			footnote
+			px-12
+			pb-12
+			absolute
+			bottom-0
+			left-0
+			w-full
+			text-xs
+			font-300
+			text-cream-100
+			hover:text-slate
+			dark:(text-slate-100
+			hover:text-cream)
+			transition-colors
+		"
+	>
 		<hr class="mb-3 border-inherit" />
 		<slot />
 	</footer>
 </template>
-
