bc166698836186da48d03307c1308557124464ae
lihbr <lihbr@users.noreply.github.com>
2022-10-12 13:54:58 +0200

    chore(deps): maintain dependencies

--- setup/shortcuts.ts
+++ setup/shortcuts.ts
@@ -1,15 +1,22 @@
-import { defineShortcutsSetup, NavOperations } from "@slidev/types";
+import {
+	defineShortcutsSetup,
+	NavOperations,
+	ShortcutOptions,
+} from "@slidev/types";
 
 import { usePip } from "../composables/usePip";
 
-export default defineShortcutsSetup((_nav: NavOperations) => {
-	return [
-		{
-			key: "ControlLeft+Backquote",
-			fn: () => {
-				usePip().cycle();
+export default defineShortcutsSetup(
+	(_nav: NavOperations, base: ShortcutOptions[]) => {
+		return [
+			...base,
+			{
+				key: "ControlLeft+Backquote",
+				fn: () => {
+					usePip().cycle();
+				},
+				autoRepeat: true,
 			},
-			autoRepeat: true,
-		},
-	];
-});
+		];
+	},
+);
