4fa460f7a1d0917aac5a337d392e836b7c8ec564
lihbr <lihbr@users.noreply.github.com>
2022-10-18 13:31:17 +0200

    fix: border color

--- components/Footnote.vue
+++ components/Footnote.vue
@@ -3,7 +3,7 @@
 	<footer
 		class="footnote px-12 pb-12 absolute bottom-0 left-0 w-full text-xs font-300 text-slate-50 text-left hover:text-slate dark:(text-slate-100 hover:text-cream) transition-colors"
 	>
-		<hr class="mb-3 border-inherit" />
+		<hr class="mb-3 border-current" />
 		<slot />
 	</footer>
 </template>
